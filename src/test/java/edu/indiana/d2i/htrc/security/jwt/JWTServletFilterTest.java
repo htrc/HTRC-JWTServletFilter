c64cd5ceab969c202f1aac7d2aa7496eba8f7924
Samitha Liyanage <samitha.liyanage@gmail.com>
2017-04-07 01:15:37 -0400

    Added signature verification for JWT token generating from WSO2IS

--- src/test/java/edu/indiana/d2i/htrc/security/jwt/JWTServletFilterTest.java
+++ src/test/java/edu/indiana/d2i/htrc/security/jwt/JWTServletFilterTest.java
@@ -47,8 +47,9 @@ public class JWTServletFilterTest {
     // mock the getRequestURI() response
     Mockito.when(mockReq.getRequestURI()).thenReturn("/");
 
+    String token = "eyJ4NXQiOiJObUptT0dVeE16WmxZak0yWkRSaE5UWmxZVEExWXpkaFpUUmlPV0UwTldJMk0ySm1PVGMxWkEiLCJraWQiOiJkMGVjNTE0YTMyYjZmODhjMGFiZDEyYTI4NDA2OTliZGQzZGViYTlkIiwiYWxnIjoiUlMyNTYifQ.eyJhdF9oYXNoIjoiRWxFMnJrYWtXMG04czJtc2dQRWxiZyIsInN1YiI6ImFkbWluIiwiYXVkIjpbIjJmWXE1WkVJeTFLbnpweFg4bkpRTnJXUHRCTWEiLCJodHRwczpcL1wvZGV2ZW52LW5vdGxzLWlzOjQ0M1wvb2F1dGgyXC90b2tlbiJdLCJyb2xlIjpbIkFwcGxpY2F0aW9uXC9hbmFseXRpY3MtZ2F0ZXdheS0xMCIsIkFwcGxpY2F0aW9uXC9hbmFseXRpY3MtZ2F0ZXdheS0yMSIsIkFwcGxpY2F0aW9uXC9hbmFseXRpY3MtZ2F0ZXdheS0yMiIsIkFwcGxpY2F0aW9uXC9hbmFseXRpY3MtZ2F0ZXdheS0yMCIsIkludGVybmFsXC9ldmVyeW9uZSIsImFkbWluIiwiQXBwbGljYXRpb25cL2FuYWx5dGljcy1nYXRld2F5LTE0IiwiQXBwbGljYXRpb25cL2FuYWx5dGljcy1nYXRld2F5LTE1IiwiQXBwbGljYXRpb25cL2FuYWx5dGljcy1nYXRld2F5LTIzIiwiQXBwbGljYXRpb25cL2FuYWx5dGljcy1nYXRld2F5LTEzIiwiQXBwbGljYXRpb25cL2FuYWx5dGljcy1nYXRld2F5LTE4IiwiQXBwbGljYXRpb25cL2FuYWx5dGljcy1nYXRld2F5LTE2IiwiQXBwbGljYXRpb25cL2FuYWx5dGljcy1nYXRld2F5LTE3Il0sImF6cCI6IjJmWXE1WkVJeTFLbnpweFg4bkpRTnJXUHRCTWEiLCJhdXRoX3RpbWUiOjE0OTE1NDExODQsImlzcyI6Imh0dHBzOlwvXC9kZXZlbnYtbm90bHMtaXM6NDQzXC9vYXV0aDJcL3Rva2VuIiwiZXhwIjoxNDkxNTc3MTg1LCJpYXQiOjE0OTE1NDExODUsImVtYWlsIjoiYWRtaW5Ad3NvMi5jb20ifQ.JTETU7Ib3EmrefbFZla246Zv-r65Ih4FvZX_bu7QzI7BbAgznUjTFTaH2tFnFOUxB5k0r0q5NMvwcHIV6KPvVui06QYReYVwjN3TZ4lJxVO0dicroIpMBkmUE6tSoUrgRqVveEZfnTEH2-p1cH9U4JodNqKF2JIcTjbiuU3EWuI";
     // mock getHeader("Authorization")
-    Mockito.when(mockReq.getHeader("Authorization")).thenReturn("Bearer " + getValidJWTToken());
+    Mockito.when(mockReq.getHeader("Authorization")).thenReturn("Bearer " + token);
 
     BufferedReader br = new BufferedReader(new StringReader("test"));
     // mock the getReader() call
@@ -68,7 +69,7 @@ public class JWTServletFilterTest {
         .sign(Algorithm.HMAC256("testsecret"));
   }
 
-  private String getResourcePath(String resource){
+  private String getResourcePath(String resource) {
     ClassLoader classLoader = getClass().getClassLoader();
     return classLoader.getResource(resource).getFile();
   }
